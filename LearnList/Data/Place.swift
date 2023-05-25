f779508dc79ec4c176ebc21135f560ad133ee682
Jevon Putra <jevon.octavian@gocement.com>
2023-05-25 23:19:52 +0700

    add model place

---
+++ LearnList/Data/Place.swift
@@ -0,0 +1,16 @@
+//
+//  Place.swift
+//  LearnList
+//
+//  Created by Gocement on 25/05/23.
+//
+import Foundation
+import SwiftUI
+
+struct Place {
+    let id: Int
+    let name: String
+    let address: String
+    let description: String
+    let image: String
+}
