e573176bac8a4a9c525dfafe7e6afc3b7eeca898
Jevon Putra <jevon.octavian@gocement.com>
2023-05-25 21:44:32 +0700

    Initial Commit

---
+++ LearnList/LearnListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnListApp.swift
+//  LearnList
+//
+//  Created by Gocement on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
